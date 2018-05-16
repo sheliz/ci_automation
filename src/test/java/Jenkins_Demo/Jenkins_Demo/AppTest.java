9bede1821c79e8d61ed5197accb2d33f4ec59c15
jonathan.aharon <jonathan.aharon@jaharon-lt.experitest.local>
2018-05-16 09:49:34 +0300

    Unique name

--- src/test/java/Jenkins_Demo/Jenkins_Demo/AppTest.java
+++ src/test/java/Jenkins_Demo/Jenkins_Demo/AppTest.java
@@ -7,22 +7,22 @@ import com.experitest.client.Client;
 import com.experitest.client.GridClient;
 
 public class AppTest {
-	//tests
+
 	protected Client client = null;
 	protected GridClient grid = null;
 	private String testName = "Jenkins Demo - Android";
-	private String accessKey = "eyJ4cC51IjoxMjcsInhwLnAiOjIsInhwLm0iOiJNVFV5TXpnM01qRTVNRFl3TnciLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE4MzkzMjYwODQsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.guEfCXtPJCKuCEMm7Xanikk2kJt-OZ2dLqBSC-8Dym8";
+	private String accessKey = System.getenv("accessKey");
 
 	@BeforeMethod
 	public void setUp(){
-		grid = new GridClient(accessKey, "sales.experitest.com",443, true);
+		grid = new GridClient(accessKey, "192.168.2.1", 9192, true);
 		client = grid.lockDeviceForExecution(testName,"@os='android'", 10, 5);
 		client.setReporter("xml", "reports", testName);
 	}
 
 	@Test
 	public void test(){
-		client.install("cloud:com.experitest.ExperiBank/.LoginActivity", true, false);
+		client.install("cloud:uniqueName=testApp", true, true);
 		client.deviceAction("Unlock");
 		client.launch("cloud:com.experitest.ExperiBank/.LoginActivity", true, false);
 		client.elementSendText("NATIVE", "id=usernameTextField", 0, "company");
